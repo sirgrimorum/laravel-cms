ca8c719ceae8100f21c72ebe92c03bca72de8850
sirgrimorum <andres.espinosa@grimorum.com>
2015-02-11 15:31:53 -0500

    arreglar checkbox y radio

--- src/views/crudgen/templates/checkbox.blade.php
+++ src/views/crudgen/templates/checkbox.blade.php
@@ -9,9 +9,9 @@ if ($dato == "") {
 }
 ?>
 @if (is_array($datos['valor']))
-@foreach($datos['valor'] as $valor)
+@foreach($datos['valor'] as $valor=>$datos2)
 <?php
-if (stripos($valor['valor'],$dato)===false){
+if (stripos($valor,$dato)===false){
     $checked = false;
 }else{
     $checked = true;
@@ -19,12 +19,12 @@ if (stripos($valor['valor'],$dato)===false){
 ?>
 <div class="checkbox">
     <label>
-        {{ Form::checkbox($columna, $valor['valor'], array('class' => 'form-control', 'id' => $tabla . '_' . $columna . '_' . $valor['valor']),$checked) }}
+        {{ Form::checkbox($columna, $valor, array('class' => 'form-control', 'id' => $tabla . '_' . $columna . '_' . $valor),$checked) }}
     </label>
-    {{ $valor['label'] }}
-    <span class="help-block" id="{{ $tabla . '_' . $columna . '_' . $valor['valor'] }}_help">
-        @if (isset($valor['description'])
-        {{ $valor['description'] }}
+    {{ $datos2['label'] }}
+    <span class="help-block" id="{{ $tabla . '_' . $columna . '_' . $valor }}_help">
+        @if (isset($datos2['description'])
+        {{ $datos2['description'] }}
         @endif
     </span>
 </div>
