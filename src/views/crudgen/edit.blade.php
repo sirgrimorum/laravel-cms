75af9f9b46d72c599cb422737f3bc53c7427c6ac
sirgrimorum <andres.espinosa@grimorum.com>
2015-02-16 15:22:14 -0500

    create con errores individuales y arreglos clases bootstrap

--- src/views/crudgen/edit.blade.php
+++ src/views/crudgen/edit.blade.php
@@ -2,9 +2,12 @@
 <div class="alert alert-info">{{ Session::pull('message') }}</div>
 @endif
 @if (count($errors->all())>0)
+<?php $errores = true ?>
+@if (isset($config['render']))
 <div class="alert alert-danger">
-{{ HTML::ul($errors->all()) }}
+    {{ HTML::ul($errors->all()) }}
 </div>
+@endif
 @endif
 <?php
 $tabla = $config['tabla'];
@@ -15,6 +18,21 @@ if (isset($config['relaciones'])) {
 }
 $identificador = $config['id'];
 $url = $config['url'];
+if (! isset($config['class_form'])) {
+    $config['class_form']='form-horizontal';
+}
+if (! isset($config['class_label'])) {
+    $config['class_label']='col-xs-12 col-sm-4 col-md-2';
+}
+if (! isset($config['class_divinput'])) {
+    $config['class_divinput']='col-xs-12 col-sm-8 col-md-10';
+}
+if (! isset($config['class_input'])) {
+    $config['class_input']='';
+}
+if (! isset($config['class_offset'])) {
+    $config['class_offset']='col-xs-offset-0 col-sm-offset-4 col-md-offset-2';
+}
 
 if (isset($config['render'])){
     $selects = array('column_name as field', 'column_type as type', 'is_nullable as null', 'column_key as key', 'column_default as default', 'extra as extra');
@@ -29,21 +47,21 @@ if (isset($config['render'])){
 }
 
 ?>
-{{ Form::model($registro, array('url' => array($url, $registro->{$identificador}), 'method' => 'PUT')) }}
+{{ Form::model($registro, array('url' => array($url, $registro->{$identificador}, 'class' => $config['class_form']), 'method' => 'PUT')) }}
     @if (isset($config['render']))
         @foreach($table_describes as $key => $columna)
             <div class="form-group">
                 @if (array_key_exists($columna->field,$campos))
                     @include("cms::crud.templates." . array_get($campos,$columna->field . ".tipo"))
                 @elseif ($columna->type == "text")
-                    {{ Form::label($columna->field, ucfirst($columna->field)) }}
-                    {{ Form::textarea($columna->field, $registro->{$columna->field}, array('class' => 'form-control')) }}
+                    {{ Form::label($columna->field, ucfirst($columna->field), array('class'=>$config['class_label']) }}
+                    {{ Form::textarea($columna->field, $registro->{$columna->field}, array('class' => 'form-control ' . $config['class_input'])) }}
                 @elseif (isset($relaciones[$columna->field]))
-                    {{ Form::label($columna->field, ucfirst($columna->field)) }}
-                    {{ Form::select($columna->field, $relaciones[$columna->field]["todos"], $registro->{$columna->field}, array('class' => 'form-control')) }}
+                    {{ Form::label($columna->field, ucfirst($columna->field), array('class'=>$config['class_label']) }}
+                    {{ Form::select($columna->field, $relaciones[$columna->field]["todos"], $registro->{$columna->field}, array('class' => 'form-control ' . $config['class_input'])) }}
                 @elseif ($columna->field != "created_at" && $columna->field != "updated_at" && $columna->key != "PRI")
-                    {{ Form::label($columna->field, ucfirst($columna->field)) }}
-                    {{ Form::text($columna->field, $registro->{$columna->field}, array('class' => 'form-control')) }}
+                    {{ Form::label($columna->field, ucfirst($columna->field), array('class'=>$config['class_label']) }}
+                    {{ Form::text($columna->field, $registro->{$columna->field}, array('class' => 'form-control ' . $config['class_input'])) }}
                 @endif
             </div>
         @endforeach
@@ -58,13 +76,25 @@ if (isset($config['render'])){
     @endif
     @if (count($botones)>0)
         @if (is_array($botones))
+        <div class="form-group">
         @foreach ($botones as $boton)
+            <div class="{{ $config['class_offset'] }} {{ $config['class_divinput'] }}">
             {{ Form::submit($boton, array('class' => 'btn btn-primary')) }}
+            </div>
         @endforeach
+        </div>
         @else
-            {{ Form::submit($botones, array('class' => 'btn btn-primary')) }}
+            <div class="form-group">
+                <div class="{{ $config['class_offset'] }} {{ $config['class_divinput'] }}">
+                    {{ Form::submit($botones, array('class' => 'btn btn-primary')) }}
+                </div>
+            </div>
         @endif
     @else
-        {{ Form::submit(Lang::get('cms::crud.create.titulo'), array('class' => 'btn btn-primary')) }}
+        <div class="form-group">
+            <div class="{{ $config['class_offset'] }} {{ $config['class_divinput'] }}">
+                {{ Form::submit(Lang::get('cms::crud.create.titulo'), array('class' => 'btn btn-primary')) }}
+            </div>
+        </div>
     @endif
 {{ Form::close() }}
